0a71ffe440321aba38b9dfd854e60343e8c13ae8
Tzu1999 <56683292+Tzu1999@users.noreply.github.com>
2020-06-04 07:57:10 +0800

    Update Role.java

--- Role.java
+++ Role.java
@@ -1,4 +1,26 @@
-
+import java.util.*;
+import java.math.*;
 public class Role {
-
+	protected int blood;
+	protected int pos_x, pos_y;
+	protected int tower_x = 200;
+	protected int tower_y = 250;
+	
+	public Role(int b, int x, int y) {
+		blood = b;
+		pos_x = x;
+		pos_y = y;
+	}
+	public int getblood() {
+		return blood;
+	}
+	public int getposx() {
+		return pos_x;
+	}
+	public int getposy() {
+		return pos_y;
+	}
+	public void setblood(int change) {
+		blood = blood + change;
+	}
 }
